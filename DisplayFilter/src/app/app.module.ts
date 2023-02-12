e86f25ecb2962705de0486889df2bef6cd2c37f8
Spakhe <spakhe2003@gmail.com>
2023-02-12 19:15:27 +0100

    Home

--- DisplayFilter/src/app/app.module.ts
+++ DisplayFilter/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
