40d672936f75393df0c5f8620a20feb601c7a987
Spakhe <spakhe2003@gmail.com>
2023-02-13 10:05:55 +0100

    Home Card Implementation

--- DisplayFilter/src/app/app.module.ts
+++ DisplayFilter/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { HomeCardComponent } from './home-card/home-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomeCardComponent
   ],
   imports: [
     BrowserModule,
